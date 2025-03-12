aa0ec82f69b370697866c72203d6d3cea7a5cf61
yutti2525 <yuu.nagahama2525@gmail.com>
2025-03-12 23:49:38 +0900

    make register login company student profiles api, and both  test is  OK

--- app/controllers/api/v1/company_profiles_controller.rb
+++ app/controllers/api/v1/company_profiles_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::CompanyProfilesController < ApplicationController
+  def create
+    @company_profile = CompanyProfile.new(company_profile_params)
+    if @company_profile.save
+      render json: @company_profile, status: :created
+    else
+      render json: @company_profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def company_profile_params
+    params.require(:company_profile).permit(:user_id, :company_overview, :business_description, :job_description)
+  end
 end
