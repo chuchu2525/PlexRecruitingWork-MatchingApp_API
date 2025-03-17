583baf607e7adbc46dcb8fcb43bcd95dedc46ec8
yutti2525 <yuu.nagahama2525@gmail.com>
2025-03-17 10:36:25 +0900

    make room controller and model, next is making message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_143847) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_150544) do
   create_table "applications", force: :cascade do |t|
     t.integer "student_profile_id", null: false
     t.integer "recruitment_id", null: false
@@ -47,6 +47,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_143847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_profile_id"], name: "index_recruitments_on_company_profile_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "student_profiles", force: :cascade do |t|
